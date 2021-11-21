e855601cd62181c3db24b9a7e12826d82c75f5cd
Alejandro UT <juretat@uni.pe>
2021-11-21 02:45:55 -0500

    Update blockchain_ureta.py

--- Modulo 1 - Crear una Cadena de Bloques/blockchain_ureta.py
+++ Modulo 1 - Crear una Cadena de Bloques/blockchain_ureta.py
@@ -6,3 +6,29 @@ Created on Sun Nov 21 01:58:50 2021
 """
 
 #Modulo 1: Crear una cadena de bloques (Blockchain)
+
+
+#Para instalar:
+    #Flask==0.12.2: pip install Flask==0.12.2
+    #Cliente HTTP Postman: https://www.getpostman.com 
+    
+    
+# Importar las librerías
+import datetime
+import hashlib           
+import json   
+from flask import Flask, jsonify
+#es la forma más rapida de enviar los datos
+
+#Parte 1 - Crear la cadena de bloques----
+#mentalidad de desarrollador, estructurar a través de clases
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof =1, previous_hash= '0')
+        
+#Parte 2 - Minado de un bloque de la cadena --------
+
+
+

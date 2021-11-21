dfa0eebdc4e5cc7f240cffeedb6587656daa85fc
Alejandro UT <juretat@uni.pe>
2021-11-21 10:02:39 -0500

    Update blockchain_ureta.py

--- Modulo 1 - Crear una Cadena de Bloques/blockchain_ureta.py
+++ Modulo 1 - Crear una Cadena de Bloques/blockchain_ureta.py
@@ -28,7 +28,45 @@ class Blockchain:
         self.chain = []
         self.create_block(proof =1, previous_hash= '0')
         
+    def create_block(self, proof, previous_hash):
+        block = {'index'  : len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash' : previous_hash }
+        self.chain.append(block)
+        return block
+    
 #Parte 2 - Minado de un bloque de la cadena --------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
